5e4db85486b01493fcec67bf75c8c46292fd374d
FloAer <ehr@etud.insa-toulouse.fr>
2021-11-12 11:46:06 +0100

    esy

---
+++ _esy/default/bin/node.cmd
@@ -0,0 +1,5 @@
+@ECHO off
+@SETLOCAL
+@SET ESY__NODE_BIN_PATH=%C:\Users\floeh\Desktop\INSA\4A\Projet_OCaml\_esy\default\bin%
+"C:\Program Files\nodejs\node.exe" -r "C:\Users\floeh\Desktop\INSA\4A\Projet_OCaml\_esy\default\pnp.js" %*
+            
