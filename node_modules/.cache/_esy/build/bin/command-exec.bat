159ea330afb5cc054f5390a7f5f7285a3a8f1b9a
Kongzibapt <baptiste1296@gmail.com>
2021-11-12 11:55:03 +0100

    install ok

--- node_modules/.cache/_esy/build/bin/command-exec.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-@SETLOCAL
-"C:\Users\floeh\AppData\Roaming\npm\node_modules\esy\3\i\esy-3dd856eb\bin\esy.exe" exec-command --include-npm-bin --include-current-env --include-build-env --project "C:\Users\floeh\Desktop\INSA\4A\Projet_OCaml" %*
